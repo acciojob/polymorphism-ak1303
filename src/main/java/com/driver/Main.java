67f3f307febc38e68195a16c61edb9a5bfc9ac5d
ak1303 <13abhi03@gmail.com>
2023-10-01 14:18:24 +0530

    compileTimePolymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,21 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public int product(int x, int y){
+            return 0;
+        }
+        public int product(int x, int y, int z){
+            return 1;
+        }
+        public double product(double x, double y){
+            return 0.0;
+        }
+    }
+ public static void main(String[] args){
+     Product p = new Product();
+     p.product(1,2);
+     p.product(3,4,5);
+     p.product(1.0,2.0);
+ }
 }
